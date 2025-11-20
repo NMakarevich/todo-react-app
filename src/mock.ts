e0a97b0e2a78452ecf2295f9b43c9800b51f13a9
NMakarevich <nikoladze529@mail.ru>
2025-11-20 15:07:32 +0300

    fix: fix mock ids

--- src/mock.ts
+++ src/mock.ts
@@ -8,19 +8,19 @@ export const TodoListMocked: TodoItemModel[] = [
     isDone: true,
   },
   {
-    id: '06595151-1ebb-4a14-ba54-73fb6db465cb',
+    id: '06595151-1ebb-4a14-ba54-73fb6db465ca',
     title: 'first todo',
     description: '',
     isDone: false,
   },
   {
-    id: '06595151-1ebb-4a14-ba54-73fb6db465cb',
+    id: '06595151-1ebb-4a14-ba54-73fb6db465cd',
     title: 'first todo',
     description: '',
     isDone: false,
   },
   {
-    id: '06595151-1ebb-4a14-ba54-73fb6db465cb',
+    id: '06595151-1ebb-4a14-ba54-73fb6db465cc',
     title: 'first todo',
     description: '',
     isDone: false,
